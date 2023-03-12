b2a0611f44ca03e039e51c4a22499b97306d1be6
Muhammad Mugni Hadi <mugnimaestra@DESKTOP-UVKMMA4>
2023-03-12 20:13:56 +0700

    add default port for fly.io

--- index.ts
+++ index.ts
@@ -40,7 +40,7 @@ app.get('*', (req: Request, res: Response) => {
   res.end('Rosa bot is running\n');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
