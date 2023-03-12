1802faf369a76808012501a2bce9327e6cf0c0a0
Muhammad Mugni Hadi <mugnimaestra@DESKTOP-UVKMMA4>
2023-03-12 20:11:22 +0700

    add pilih feature function

--- bot.ts
+++ bot.ts
@@ -1,5 +1,6 @@
 import { Client, TextMessage, MessageEvent, WebhookEvent } from '@line/bot-sdk';
 import dotenv from 'dotenv';
+import { Mention, MentionTextMessage } from './types.js';
 
 if (!process.env.LINE_CHANNEL_SECRET) {
   dotenv.config();
@@ -11,11 +12,47 @@ export const client = new Client({
 });
 
 // function for creating a reply message
-export const createTextMessage = (message: string): TextMessage => {
+export const createTextMessage = (
+  message: string,
+  mentions?: Mention[]
+): MentionTextMessage => {
   return {
     type: 'text',
     text: message,
+    mention: mentions
+      ? {
+          mentionees: mentions,
+        }
+      : undefined,
   };
+};
+
+const mainFunction = (event: MessageEvent): void => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text messages
+    return;
+  }
+  let message;
+  if (event.message.text.startsWith('!pilih')) {
+    // Split the input string by the "atau" keyword
+    const options = event.message.text.substring(7).split('atau');
+
+    // Trim whitespace from both options
+    const optionA = options[0].trim();
+    const optionB = options[1].trim();
+
+    // Randomly pick an option with a 50% chance for each option
+    const randomNum = Math.random();
+    if (randomNum < 0.5) {
+      message = optionA;
+    } else {
+      message = optionB;
+    }
+  }
+
+  if (message) {
+    client.replyMessage(event.replyToken, createTextMessage(message));
+  }
 };
 
 // function for handling incoming text messages
@@ -26,37 +63,21 @@ export const handleTextMessage = (event: MessageEvent): void => {
   }
 
   const userId = event.source.userId;
-  const message = event.message.text;
-
-  const replyMessage = createTextMessage(message);
 
   if (process.env.NODE_ENV === 'development') {
     if (userId === 'Ua684ecb5c5d077e54d95a1d3ebaae15a') {
-      client
-        .replyMessage(event.replyToken, replyMessage)
-        .then(() => {
-          console.log(`Sent reply message to user: ${userId}`);
-        })
-        .catch(err => {
-          console.error(err);
-        });
+      mainFunction(event);
     }
   } else {
-    client
-      .replyMessage(event.replyToken, replyMessage)
-      .then(() => {
-        console.log(`Sent reply message to user: ${userId}`);
-      })
-      .catch(err => {
-        console.error(err);
-      });
+    mainFunction(event);
   }
 };
 
 // function for handling unsend events
-export const handleUnsendEvent = (event: WebhookEvent): void => {
+export const handleUnsendEvent = async (event: WebhookEvent): Promise<void> => {
   console.log(`Unsend event received: ${JSON.stringify(event)}`);
   let target;
+  let displayName;
 
   switch (event.source.type) {
     case 'user':
@@ -73,6 +94,16 @@ export const handleUnsendEvent = (event: WebhookEvent): void => {
   }
 
   if (target) {
-    client.pushMessage(target, createTextMessage('Ih apaan tuh diunsent'));
+    if (event.source.userId) {
+      await client.getProfile(event.source.userId).then(res => {
+        console.log('user profile info', res);
+        displayName = res.displayName;
+      });
+    }
+
+    client.pushMessage(
+      target,
+      createTextMessage(`Wah, ${displayName ?? 'anon'} unsent apa tuh`)
+    );
   }
 };
