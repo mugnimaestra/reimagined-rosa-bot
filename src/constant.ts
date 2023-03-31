e2dee20c48ec23e18d649a70620bdab6a6b3269d
Muhammad Mugni Hadi <mugnimaestra@DESKTOP-UVKMMA4>
2023-03-31 21:02:08 +0700

    feat: update regex pattern to be more simplier

--- src/constant.ts
+++ src/constant.ts
@@ -2,4 +2,4 @@ export const reelUrlRegex = new RegExp(
   /(?:https?:\/\/(?:www\.)?instagram\.com\/(?:reels|reel)\/(?:[A-Za-z0-9._%+-]+\/)?)([^#\?]+)/g
 );
 
-export const teraboxUrlRegex = new RegExp(/https:\/\/teraboxapp\.com\/s\/\w+/);
+export const teraboxUrlRegex = new RegExp(/https:\/\/teraboxapp\.com\/s\/.+/);
